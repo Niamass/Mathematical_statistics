374009c8c18368d95f38c4cd0db327446b4999eb
Niamass <60889507+Niamass@users.noreply.github.com>
2021-03-10 19:34:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,82 +1,69 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.stats as sts
 import math as m
 
-size = [10, 50, 1000]
+size = [10, 100, 1000]
+
+def values(sample, average, med, zr, zq, ztr):
+    sample.sort()
+    average.append(np.average(sample))#выборочное среднее
+    med.append(np.median(sample))#выборочная медиана
+    zr.append((min(sample) + max(sample))/2)#Полусумма экстремальных выборочных элементов
+    zq.append((np.quantile(sample, 0.25) + np.quantile(sample, 0.75))/2)#Полусумма квартилей
+    ztr.append(np.mean(sample[len(sample)//4: len(sample)-len(sample)//4]))#Усечённое среднее
+
+def print_E_D(all_v, s):
+    for i in all_v:
+        E = round(np.mean(i), 6)
+        D = round(np.std(i) ** 2, 6)
+        print("size = ", s)
+        print(E, D, round(E - D ** 0.5, 6), round(E + D ** 0.5, 6))
+
 
 #Нормальное распределение
 for s in size:
-    density = sts.norminvgauss(1, 0)  # плотность
-    hist_sample = sts.norminvgauss.rvs(1, 0, size=s)  # выборка для гистограммы
-
-    fig, ax = plt.subplots()
-    ax.hist(hist_sample, density=True)  # гистограмма
-    x = np.linspace(density.ppf(0.01), density.ppf(0.99), 100)
-    ax.plot(x, density.pdf(x), 'k-', lw=2)  # плотность
-    ax.set_xlabel("Normal")
-    ax.set_ylabel("Density")
-    ax.set_title("Sample size = " + str(s))
-    plt.show()
-
+    average, med, zr, zq, ztr = [], [], [], [], []
+    all_v = [average, med, zr, zq, ztr]
+    for i in range(1000):
+        sample = sts.norminvgauss.rvs(1, 0, size=s)
+        values(sample, average, med, zr, zq, ztr)
+    print_E_D(all_v, s)
 
 #Распределение Коши
 for s in size:
-    density = sts.cauchy()  # плотность
-    hist_sample = sts.cauchy.rvs(size=s)  # выборка для гистограммы
-
-    fig, ax = plt.subplots()
-    ax.hist(hist_sample, density=True)  # гистограмма
-    x = np.linspace(density.ppf(0.01), density.ppf(0.99), 100)
-    ax.plot(x, density.pdf(x), 'k-', lw=2)  # плотность
-    ax.set_xlabel("Cauchy")
-    ax.set_ylabel("Density")
-    ax.set_title("Sample size = " + str(s))
-    plt.show()
-
+    average, med, zr, zq, ztr = [], [], [], [], []
+    all_v = [average, med, zr, zq, ztr]
+    for i in range(1000):
+        sample = sts.cauchy.rvs(size=s)
+        values(sample, average, med, zr, zq, ztr)
+    print_E_D(all_v, s)
 
 #Распределение Лапласа
 for s in size:
-    density = sts.laplace(loc=0, scale=1 / m.sqrt(2))  # плотность
-    hist_sample = sts.laplace.rvs(loc=0, scale=1 / m.sqrt(2), size=s)  # выборка для гистограммы
-
-    fig, ax = plt.subplots()
-    ax.hist(hist_sample, density=True)  # гистограмма
-    x = np.linspace(density.ppf(0.01), density.ppf(0.99), 100)
-    ax.plot(x, density.pdf(x), 'k-', lw=2)  # плотность
-    ax.set_xlabel("Laplace")
-    ax.set_ylabel("Density")
-    ax.set_title("Sample size =" + str(s))
-    plt.show()
-
+    average, med, zr, zq, ztr = [], [], [], [], []
+    all_v = [average, med, zr, zq, ztr]
+    for i in range(1000):
+        sample = sts.laplace.rvs(loc=0, scale=1 / m.sqrt(2), size=s)
+        values(sample, average, med, zr, zq, ztr)
+    print_E_D(all_v, s)
 
 #Распределение Пуассона
 for s in size:
-    density = sts.poisson(mu=10)  # плотность
-    hist_sample = sts.poisson.rvs(mu=10, size=s)  # выборка для гистограммы
-
-    fig, ax = plt.subplots()
-    ax.hist(hist_sample, density=True)  # гистограмма
-    x = np.arange(sts.poisson.ppf(0.01, mu=10), sts.poisson.ppf(0.99, mu=10))
-    ax.plot(x, density.pmf(x), 'k-', lw=2)  # плотность
-    ax.set_xlabel("Poisson")
-    ax.set_ylabel("Density")
-    ax.set_title("Sample size = " + str(s))
-    plt.show()
+    average, med, zr, zq, ztr = [], [], [], [], []
+    all_v = [average, med, zr, zq, ztr]
+    for i in range(1000):
+        sample = sts.poisson.rvs(mu=10, size=s)
+        values(sample, average, med, zr, zq, ztr)
+    print_E_D(all_v, s)
 
 #Равномерное распределение
 for s in size:
-    density = sts.uniform(-m.sqrt(3), 2 * m.sqrt(3))  # плотность
-    hist_sample = sts.uniform.rvs(-m.sqrt(3), 2 * m.sqrt(3), size=s)  # выборка для гистограммы
-
-    fig, ax = plt.subplots()
-    ax.hist(hist_sample, density=True)  # гистограмма
-    x = np.linspace(density.ppf(0.01), density.ppf(0.99), 100)
-    ax.plot(x, density.pdf(x), 'k-', lw=2)  # плотность
-    ax.set_xlabel("Uniform")
-    ax.set_ylabel("Density")
-    ax.set_title("Sample size = " + str(s))
-    plt.show()
+    average, med, zr, zq, ztr = [], [], [], [], []
+    all_v = [average, med, zr, zq, ztr]
+    for i in range(1000):
+        sample = sts.uniform.rvs(-m.sqrt(3), 2 * m.sqrt(3), size=s)
+        values(sample, average, med, zr, zq, ztr)
+    print_E_D(all_v, s)
 
 
 
